e8a58e492492cf94f0c59012a0662c037ac8f599
Alexander Gård <alex.gaard@hotmail.com>
2022-03-29 14:20:07 +0200

    Fjernet ubrukt scope og la til context path

--- src/main/kotlin/no/nav/poao_openam_proxy/config/properties/EnvironmentProperties.kt
+++ src/main/kotlin/no/nav/poao_openam_proxy/config/properties/EnvironmentProperties.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "app.env")
 data class EnvironmentProperties (
 	var mulighetsrommetApiUrl: String = "",
-	var mulighetsrommetApiScope: String = "",
 	var openAmDiscoveryUrl: String = "",
 	var veilarbloginOpenAmClientId: String = "",
 )
